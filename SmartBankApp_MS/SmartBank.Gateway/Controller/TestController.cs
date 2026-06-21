3e3e9c9785e4b65818a1ef318236b07ce190fda1
Surya Kanta Dalai <suryakantadlai22@gmail.com>
2026-06-21 13:52:02 +0530

    Project working

---
+++ SmartBankApp_MS/SmartBank.Gateway/Controller/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartBank.Gateway.Controller
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Gateway Working");
+        }
+    }
+}

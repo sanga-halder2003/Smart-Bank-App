3f2198f30f5cc2a78dfe7beb5281239b13cb7e8b
SaiPriyaReddy <saipriyareddy530@gmail.com>
2026-06-23 15:15:50 +0530

    Added RabbitMQ consumer integration

---
+++ SmartBankApp_MS/SmartBank.CustomerServie/Messaging/RabbitMQConsumer.cs
@@ -0,0 +1,81 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using SmartBank.CustomerServie.Models;
+using SmartBank.CustomerServie.Repositories;
+using System.Text;
+using System.Text.Json;
+
+namespace SmartBank.CustomerServie.Messaging
+{
+    public class RabbitMQConsumer
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public RabbitMQConsumer(
+            IConfiguration configuration,
+            IServiceScopeFactory scopeFactory)
+        {
+            _configuration = configuration;
+            _scopeFactory = scopeFactory;
+        }
+
+        public async Task StartConsumingAsync()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _configuration["RabbitMQ:Host"],
+                Port = int.Parse(_configuration["RabbitMQ:Port"]!),
+                UserName = _configuration["RabbitMQ:UserName"],
+                Password = _configuration["RabbitMQ:Password"]
+            };
+
+            var connection = await factory.CreateConnectionAsync();
+            var channel = await connection.CreateChannelAsync();
+
+            var queueName = _configuration["RabbitMQ:QueueName"];
+
+            await channel.QueueDeclareAsync(
+                queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                var userEvent =
+                    JsonSerializer.Deserialize<UserRegisteredEvent>(message);
+
+                if (userEvent != null)
+                {
+                    using var scope = _scopeFactory.CreateScope();
+
+                    var repository =
+                        scope.ServiceProvider
+                             .GetRequiredService<ICustomerRepository>();
+
+                    var customer = new Customer
+                    {
+                        FullName = userEvent.FullName,
+                        Email = userEvent.Email,
+                        Phone = "",
+                        Address = ""
+                    };
+
+                    await repository.AddAsync(customer);
+                }
+            };
+
+            await channel.BasicConsumeAsync(
+                queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+        }
+    }
+}
--- SmartBankApp_MS/SmartBank.CustomerServie/Program.cs
+++ SmartBankApp_MS/SmartBank.CustomerServie/Program.cs
@@ -2,12 +2,13 @@ using Microsoft.EntityFrameworkCore;
 using SmartBank.CustomerServie.Data;
 using SmartBank.CustomerServie.Repositories;
 using SmartBank.CustomerServie.Services;
+using SmartBank.CustomerServie.Messaging;
 
 namespace SmartBank.CustomerServie
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -18,14 +19,24 @@ namespace SmartBank.CustomerServie
                 options.UseSqlServer(
                     builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            // Dependency Injection
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            builder.Services.AddScoped<
+                ICustomerService,
+                SmartBank.CustomerServie.Services.CustomerService>();
+
+            builder.Services.AddSingleton<RabbitMQConsumer>();
+
             // Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<
-    ICustomerService,
-    SmartBank.CustomerServie.Services.CustomerService>();
+
             var app = builder.Build();
+
+            // Start RabbitMQ Consumer
+            var consumer = app.Services.GetRequiredService<RabbitMQConsumer>();
+            await consumer.StartConsumingAsync();
 
             // Configure Pipeline
             if (app.Environment.IsDevelopment())
@@ -40,7 +51,7 @@ builder.Services.AddScoped<
 
             app.MapControllers();
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }

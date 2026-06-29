67747a20564f4a7f06e594637789f1e9ac93aaf6
SaiPriyaReddy <saipriyareddy530@gmail.com>
2026-06-29 17:09:12 +0530

    Added global exception middleware and Serilog logging

--- SmartBankApp_MS/SmartBank.CustomerServie/Program.cs
+++ SmartBankApp_MS/SmartBank.CustomerServie/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using SmartBank.CustomerServie.Data;
+using SmartBank.CustomerServie.Messaging;
+using SmartBank.CustomerServie.Middleware;
 using SmartBank.CustomerServie.Repositories;
 using SmartBank.CustomerServie.Services;
-using SmartBank.CustomerServie.Messaging;
 
 namespace SmartBank.CustomerServie
 {
@@ -10,35 +12,48 @@ namespace SmartBank.CustomerServie
     {
         public static async Task Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/log-.txt",
+                    rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseSerilog();
+
+            // Add Services
             builder.Services.AddControllers();
 
             builder.Services.AddDbContext<CustomerDbContext>(options =>
                 options.UseSqlServer(
                     builder.Configuration.GetConnectionString("DefaultConnection")));
 
-     
+            // Dependency Injection
             builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
             builder.Services.AddScoped<
                 ICustomerService,
                 SmartBank.CustomerServie.Services.CustomerService>();
 
-            builder.Services.AddScoped<ICustomerPublisher, CustomerPublisher>();
+            builder.Services.AddScoped<
+                ICustomerPublisher,
+                CustomerPublisher>();
 
             builder.Services.AddSingleton<RabbitMQConsumer>();
 
-
+            // Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            // Global Exception Middleware
+            app.UseCustomExceptionMiddleware();
 
+            // Start RabbitMQ Consumer
             var consumer = app.Services.GetRequiredService<RabbitMQConsumer>();
             await consumer.StartConsumingAsync();
-
 
             if (app.Environment.IsDevelopment())
             {

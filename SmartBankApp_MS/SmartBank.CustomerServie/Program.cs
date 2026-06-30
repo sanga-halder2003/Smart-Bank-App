1aef11ac01ef3eee52aad6028e3145a6659f4cc7
Sanga Halder <sangahalder2610@gmail.com>
2026-06-30 22:00:30 +0530

    Revert "Update completed services except transaction"
    This reverts commit f87108e350ca9395ad3f1ddfd23c2d9fa0f6fedb.

--- SmartBankApp_MS/SmartBank.CustomerServie/Program.cs
+++ SmartBankApp_MS/SmartBank.CustomerServie/Program.cs
@@ -1,17 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using SmartBank.CustomerServie.Data;
+using SmartBank.CustomerServie.Messaging;
+using SmartBank.CustomerServie.Middleware;
+using SmartBank.CustomerServie.Repositories;
+using SmartBank.CustomerServie.Services;
+
 namespace SmartBank.CustomerServie
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/log-.txt",
+                    rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseSerilog();
+
+            // Add Services
             builder.Services.AddControllers();
 
+            builder.Services.AddDbContext<CustomerDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Dependency Injection
+            builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            builder.Services.AddScoped<
+                ICustomerService,
+                SmartBank.CustomerServie.Services.CustomerService>();
+
+            builder.Services.AddScoped<
+                ICustomerPublisher,
+                CustomerPublisher>();
+
+            builder.Services.AddSingleton<RabbitMQConsumer>();
+
+            // Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            // Global Exception Middleware
+            app.UseCustomExceptionMiddleware();
+
+            // Start RabbitMQ Consumer
+            var consumer = app.Services.GetRequiredService<RabbitMQConsumer>();
+            await consumer.StartConsumingAsync();
 
             if (app.Environment.IsDevelopment())
             {
@@ -25,7 +67,7 @@ namespace SmartBank.CustomerServie
 
             app.MapControllers();
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }
